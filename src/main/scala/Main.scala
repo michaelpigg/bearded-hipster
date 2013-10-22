9f73ee2ef709e16e5cd38e14b4272c1ae11a5b47
Michael Pigg <mikepigg@highlylogical.com>
2013-10-22 18:57:32 -0400

    don't iterate in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,8 +2,7 @@ import java.net.NetworkInterface
 
 object Main extends App {
   val nis = NetworkInterface.getNetworkInterfaces
-  while (nis.hasMoreElements) {
-    val ni = nis.nextElement()
-    println("if " + ni.getName)
-    println("hw " + ni.getHardwareAddress)
-  }}
+  val ni = nis.nextElement()
+  println("if " + ni.getName)
+  println("hw " + ni.getHardwareAddress)
+}
