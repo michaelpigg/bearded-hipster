9d53e46ae1a7cc861e49b59d7226f8ba625dde82
Michael Pigg <mikepigg@highlylogical.com>
2013-10-22 20:40:17 -0400

    Call java from main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,9 @@
 import java.net.NetworkInterface
 
 object Main extends App {
-  val nis = NetworkInterface.getNetworkInterfaces
-  val ni = nis.nextElement()
-  println("if " + ni.getName)
-  println("hw " + ni.getHardwareAddress)
+//  val nis = NetworkInterface.getNetworkInterfaces
+//  val ni = nis.nextElement()
+//  println("if " + ni.getName)
+//  println("hw " + ni.getHardwareAddress)
+  JavaNetworkInterface.iterateInterfaces()
 }
