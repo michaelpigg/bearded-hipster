a52e8192923914bc3bdf5289383f85b9df22d689
Michael Pigg <mikepigg@highlylogical.com>
2013-10-23 05:12:27 -0400

    Only java source

--- build.sbt
+++ build.sbt
@@ -3,7 +3,3 @@ scalaVersion := "2.10.2"
 fork := true
 
 retrieveManaged := true
-
-libraryDependencies ++= Seq(
- "org.scalatest" %% "scalatest" % "2.0.RC2"   % "test"
- )
--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-import java.net.NetworkInterface
-
-object Main {
-//  val nis = NetworkInterface.getNetworkInterfaces
-//  val ni = nis.nextElement()
-//  println("if " + ni.getName)
-//  println("hw " + ni.getHardwareAddress)
-//  JavaNetworkInterface.iterateInterfaces()
-}
